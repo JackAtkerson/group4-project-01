c83e23867e3b69b8397a6a23eb11830df10d3b99
Jack Atkerson <jatkerson18@gmail.com>
2022-06-05 16:18:43 -0500

    Map functionality adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var zipcodeInputEl = document.querySelector("#zipcode");
 var breweriesContainer = document.querySelector("#breweries-container");
+var mapContainerEl = document.querySelector("#map");
 var showBreweries = document.querySelector("showbreweries");
 
 var formSubmitHandler = function(event) {
@@ -11,23 +12,21 @@ var formSubmitHandler = function(event) {
     var zipcode = zipcodeInputEl.value.trim();
   
     if (zipcode) {
-        getBreweriesList(zipcode)
-  
-        getBreweriesList(zipcode);
-      // clear old content
-      breweriesContainer.textContent = "";
-      zipcodeInputEl.value = "";
+      getBreweriesList(zipcode)
+        breweriesContainer.textContent = "";
+        zipcodeInputEl.value = "";
+        mapContainerEl.textContent = "";
     } else {
       alert("Please enter a valid Zipcode");
     }
 };
+
 var Coordinates = [];
+
 var getBreweriesList = function(breweries) {
-    
   var apiUrl = "https://api.openbrewerydb.org/breweries?by_postal=" + breweries; 
     fetch(apiUrl)
       .then(function(response) {
-        
         if (response.ok) {
           console.log(response);
           response.json().then(function(data) {
@@ -39,7 +38,7 @@ var getBreweriesList = function(breweries) {
               Coordinates.push(Passthrough);
             }
             displayBreweries(data, breweries);
-            displayMap(data, breweries);
+            //displayMap(data, breweries);
           });
         } else {
           alert('Error: Breweries Not Found');
@@ -50,126 +49,77 @@ var getBreweriesList = function(breweries) {
       });
 
       console.log(apiUrl);
-      AddCircles();
+      //AddCircles();
 };
 
 var displayBreweries = function(breweries) {
-    
-    if (breweries.length === 0) {
-      breweriesContainer.textContent = "No breweries found.";
-      return;
-    }
+  if (breweries.length === 0) {
+    breweriesContainer.textContent = "No breweries found.";
+    return;
+  };
 
-    var map = new ol.Map({
-      target: 'map',
-      layers: [
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
         new ol.layer.Tile({
-          source: new ol.source.OSM(0,0)
+            source: new ol.source.OSM()
         })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([breweryLongitude, breweryLatitiude]),
-        zoom: 16
-      })
-    });
-
-    for (var i = 0; i < breweries.length; i++) {
-      
-        var breweryName = breweries[i].name;
-        var brewerySite = breweries[i].website_url;
-        var breweryAddress = breweries[i].street;
-        var breweryLongitude = breweries[i].longitude;
-        var breweryLatitiude = breweries[i].latitude;
-
-        console.log(breweryName);
-
-        console.log(breweryLongitude);
-        console.log(breweryLatitiude);
+    ],
+    view: new ol.View({
+        projection:"EPSG:4326",
+        center: [breweries[0].longitude, breweries[0].latitude],
+        zoom: 16,
+        minzoom: 13,
+        maxzoom: 20            
+    })
+  });
   
-        var breweryEl = document.createElement("a");
-        breweryEl.classList = "list-item flex-row justify-space-between align-center";
-        breweryEl.setAttribute("href", brewerySite);
-        breweryEl.setAttribute("target", "_blank");
+  for (var i = 0; i < breweries.length; i++) {
     
-        var titleEl = document.createElement("span");
-        titleEl.textContent = breweryName;
+    var breweryName = breweries[i].name;
+    var brewerySite = breweries[i].website_url;
+    var breweryAddress = breweries[i].street;
+    var breweryLongitude = breweries[i].longitude;
+    var breweryLatitiude = breweries[i].latitude;
 
-        var breweryInfoEl = document.createElement("a");
-        breweryInfoEl.setAttribute("href", brewerySite);
-        breweryInfoEl.setAttribute("target", "_blank");
-        breweryInfoEl.textContent = "";
-        breweryInfoEl.setAttribute("class", "brewery-info btn button is-rounded is-responsive");
-
-        var addressEl = document.createElement("p");
-        addressEl.textContent = "Address: " + breweryAddress;
-
-        breweryInfoEl.appendChild(titleEl);
-        breweryInfoEl.appendChild(addressEl);
-        breweriesContainer.appendChild(breweryInfoEl);
+    console.log(breweryName);
+    console.log(breweryLongitude);
+    console.log(breweryLatitiude);
+  
+    var breweryEl = document.createElement("a");
+      breweryEl.classList = "list-item flex-row justify-space-between align-center";
+      breweryEl.setAttribute("href", brewerySite);
+      breweryEl.setAttribute("target", "_blank");
     
-        var centerLongitudeLatitude = ol.proj.fromLonLat([breweryLongitude, breweryLatitiude]);
-        var layer = new ol.layer.Vector({
-          source: new ol.source.Vector({
-            projection: 'EPSG:4326',
-            features: [new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, 50))]
-          }),
-          style: [
-            new ol.style.Style({
-              stroke: new ol.style.Stroke({
-                color: 'blue',
-                width: 1
-              }),
-              fill: new ol.style.Fill({
-                color: 'rgba(0, 0, 255, 0.1)'
-              })
-            })
-          ]
-        });
-    
-        map.addLayer(layer);
-    }
+    var titleEl = document.createElement("span");
+      titleEl.textContent = breweryName;
 
-    
+    var breweryInfoEl = document.createElement("a");
+      breweryInfoEl.setAttribute("href", brewerySite);
+      breweryInfoEl.setAttribute("target", "_blank");
+      breweryInfoEl.textContent = "";
+      breweryInfoEl.setAttribute("class", "brewery-info btn button is-rounded is-responsive");
 
+    var addressEl = document.createElement("p");
+      addressEl.textContent = "Address: " + breweryAddress;
+
+    breweryInfoEl.appendChild(titleEl);
+    breweryInfoEl.appendChild(addressEl);
+    breweriesContainer.appendChild(breweryInfoEl);
+
+    var element = document.createElement('div');
+      element.innerHTML = '<img src="https://cdn.mapmarker.io/api/v1/fa/stack?size=50&color=DC4C3F&icon=fa-microchip&hoffset=1" />';
+    
+    var marker = new ol.Overlay({
+      position: [breweries[i].longitude, breweries[i].latitude],
+      positioning: 'center-center',
+      element: element,
+      stopEvent: false
+    });
+    
+    map.addOverlay(marker);
   };
-
-var displayMap = function(breweries){
-    console.log("map");
-  };
+}; 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
-//* Start of Open Layers Java coding *//
-
-
-// var Coordinates = [[-90.22073676,38.6348785],[-90.20969425,38.63298865]]
-function AddCircles (){
-  console.log(Coordinates.length)
-for (let i = 0;i < Coordinates.length; i++){
-  var Long = Coordinates[i][0]
-  var Lat = Coordinates[i][1]
-  var Coords = [] 
-  Coords.push(Long)
-  Coords.push(Lat) 
-  var centerLongitudeLatitude = ol.proj.fromLonLat(Coords);
-  var layer = new ol.layer.Vector({
-
-    source: new ol.source.Vector({
-      projection: 'EPSG:4326',
-      features: [new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, 5000))]
-    }),
-    style: [
-      new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          color: 'blue',
-          width: 1
-        }),
-        fill: new ol.style.Fill({
-          color: 'rgba(0, 0, 255, 0.1)'
-        })
-      })
-    ]
-  });
-  map.addLayer(layer);
-}
-}
